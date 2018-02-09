d2f6f3c4022465dd1b52f3604d19780e2d56bcd8
Carlos Lobato Calabozo <karlos_lobato@hotmail.com>
2018-02-09 16:15:07 +0100

    <ADD> test_module

--- odoo/custom/src/private/test_module/__manifest__.py
+++ odoo/custom/src/private/test_module/__manifest__.py
@@ -1,5 +1,6 @@
 #Manifest example.
 {
+    'author':'Campo y Tierra del Jerte S.A.',
     'name':'Test Module',
     'depends':[
         'base',
@@ -9,7 +10,14 @@
     'description':"""
         This is an *extended description* for the module that is testing our
         skills developing in **odoo**. With this we learn:
+        
         * Basic module
         * How to install modules
+        * Types of 
     """,
+    'license':'AGPL-3',
+    'website':'https://www.campoytierra.com',
+    'version':'11.0.1.0.0',
+    'category':'Tools',
 }
+
