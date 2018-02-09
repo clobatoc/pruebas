74071949d829302107bb68c38a8edcceaaaa75a2
Carlos Lobato Calabozo <karlos_lobato@hotmail.com>
2018-02-09 12:37:44 +0100

    Initial Commit Docker Odoo Base Develop Configuration

---
+++ odoo/custom/src/private/test_module/__manifest__.py
@@ -0,0 +1,15 @@
+#Manifest example.
+{
+    'name':'Test Module',
+    'depends':[
+        'base',
+        'account',
+    ],
+    'summary':'Module for testing odooo',
+    'description':"""
+        This is an *extended description* for the module that is testing our
+        skills developing in **odoo**. With this we learn:
+        * Basic module
+        * How to install modules
+    """,
+}
